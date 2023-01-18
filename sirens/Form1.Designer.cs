e5a3748216ac8b1720bd94b586e4f8f139e0054c
Musiienko Anton <sa090550@gmail.com>
2023-01-18 11:20:32 +0200

    Add project files.

---
+++ sirens/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace sirens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(180, 469);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 569);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Підтвердити (старт) свій вибір";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 500);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Виводити звук";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(140, 484);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 484);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Час оновлення:";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 523);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(151, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Вивід спливаючого вікна";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(12, 546);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(100, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Зміна кольора";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LawnGreen;
+            this.ClientSize = new System.Drawing.Size(204, 601);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(220, 640);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(220, 640);
+            this.Name = "Form1";
+            this.Text = "Повітряна тревога";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ sirens/Form1.cs
@@ -0,0 +1,182 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.Net;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace sirens
+{
+    public partial class Form1 : Form
+    {
+
+
+        void changeBackColor(Color color)
+        {
+            this.BackColor = color;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = Int32.Parse(textBox1.Text) * 1000;
+            aTimer.Enabled = true;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            var url = "https://sirens.in.ua/api/v1/";
+
+
+
+
+
+            using (WebClient webClient = new WebClient())
+            {
+
+                var r = webClient.DownloadString(url);
+                dynamic dynObj = JsonConvert.DeserializeObject(r);
+
+                JObject obj = JObject.Parse(r);               // Parse the JSON to a JObject
+                foreach (var item in obj)
+                {
+                    checkedListBox1.Items.Add(item.Key);
+                }
+                foreach (var item in obj)
+                {
+                    if (item.Value.ToString() != "full" && item.Value.ToString() != "no_data")
+                    {
+                        Console.WriteLine("Область: {0}, статус: {1}", item.Key, "Відсутня");
+                    }
+                    else if (item.Value.ToString() == "full")
+                    {
+                        Console.WriteLine("Область: {0}, статус: {1}", item.Key, "Тривога");
+                    }
+                    else if (item.Value.ToString() == "partial")
+                    {
+                        Console.WriteLine("Область: {0}, статус: {1}", item.Key, "Часткова тривога");
+                    }
+                    else if (item.Value.ToString() == "no_data")
+                    {
+                        Console.WriteLine("Область: {0}, статус: {1}", item.Key, "Нема даних");
+                    }
+                }
+
+            }
+            //Console.WriteLine(ress);
+
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(checkedListBox1.SelectedItem);
+          
+                this.BackColor = Color.LawnGreen;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //MessageBox.Show(textBox1.Text.ToString(), "Час оновлення", MessageBoxButtons.OK);
+            Console.WriteLine("Час оновлення: {0}", textBox1.Text.ToString());
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = Int32.Parse(textBox1.Text) * 1000;
+            aTimer.Enabled = true;
+        }
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            
+            Console.WriteLine("Таймер працює");
+            var url = "https://sirens.in.ua/api/v1/";
+            if (!checkBox3.Checked)
+            {
+                this.BackColor = Color.LawnGreen;
+            }
+            using (WebClient webClient = new WebClient())
+            {
+               
+                var r = webClient.DownloadString(url);
+                dynamic dynObj = JsonConvert.DeserializeObject(r);
+
+                JObject obj = JObject.Parse(r);
+                foreach (var item in obj)
+                {
+                    try
+                    {
+
+                        foreach (var item2 in checkedListBox1.CheckedItems)
+                        {
+                            if (item2.ToString() == item.Key && (item.Value.ToString() == "full" || item.Value.ToString() == "partial"))
+                            {
+                                //  try
+                                //  {
+                                //      changeBackColor(Color.Maroon);
+                                //  }
+                                //  catch (FormatException exp)
+                                //  {
+                                //      Console.WriteLine(exp.Message);
+                                //  }
+                                if (checkBox1.Checked)
+                                {
+                                    System.Media.SystemSounds.Asterisk.Play();
+                                }
+                                if (checkBox3.Checked)
+                                {
+                                    this.BackColor = Color.Red;
+                                }
+                                if (!checkBox3.Checked)
+                                {
+                                    this.BackColor = Color.LawnGreen;
+                                }
+                                if (checkBox2.Checked)
+                                {
+                                    MessageBox.Show("В регіоні: " + item.Key + " , оголошена ТРИВОГА! Прямуйте до найближчого укриття! Будьте обачні!", "В вибраному регіоні тривога!!!", MessageBoxButtons.OKCancel);
+
+                                }
+
+                            }
+
+
+
+                            // else changeBackColor(Color.LawnGreen);
+                        }
+                    }
+                    catch (FormatException exp)
+                    {
+                        Console.WriteLine(exp.Message);
+                    }
+                }
+
+
+
+           
+
+
+
+
+
+
+
+            }
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox3.Checked)
+            {
+                this.BackColor = Color.LawnGreen;
+            }
+        }
+    }
+}
+
